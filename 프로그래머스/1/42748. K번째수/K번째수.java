ab0222d23291d375bdf54ddd869165f89a721907
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-01-19 23:56:55 +0900

    [level 1] Title: K번째수, Time: 0.61 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,34 +1,29 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-
+        int[] answer= new int[commands.length];
+        
         /*
-        k번째
-        I~J번쨰 정렬하고 k번째수
-        */
-        /*
-        0행 1열부터 0행 2열의 차이+1 만큼의 배열을 선언
-        0행 1열부터 0행 2열 +1 까지의 값을 새로운 배열에 가져옴
-        새로운 배열을 정렬
-        0행 3열 번째 수를 추출해서 answer에 넣기
+        값을 넣을 새로운 배열 생성. 크기는 commands 행의 수
+        i번째부터 j번째까지 정렬하고 k번째 수
+        0행0열 번째 부터 0행1열 번째까지
+        i-1               j-1            k-1            인덱스
+        추출하면 새로운 배열에 넣기
+        
         */
         
-        for(int k=0; k<commands.length; k++){
-            int[] newArr = new int[Math.abs(commands[k][1]-commands[k][0])+1];
-            System.out.print(newArr.length);
-            int j=0;
-            for(int i=commands[k][0]-1; i<commands[k][1]; i++){
-                System.out.println(newArr.length);
-                newArr[j] = array[i];
-                j++;
+
+        for(int n=0;n<commands.length; n++){
+            int num=0;
+            int[] newArr = new int[Math.abs(commands[n][0]-commands[n][1])+1];
+            for(int m=commands[n][0]-1; m<commands[n][1]; m++){
+                newArr[num]=array[m];
+                num++;
             }
             Arrays.sort(newArr);
-            answer[k]=newArr[commands[k][2]-1];
+            answer[n]=newArr[commands[n][2]-1];
         }
 
-        
-        
         return answer;
     }
 }
