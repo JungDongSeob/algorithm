810b1f929a56ba96efe7e64cf46d7a3fd86fd39b
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-01-21 16:35:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.42 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -4,35 +4,25 @@ public class Solution {
     public int[] solution(int []arr) {
 
         
-        /*
-        list생성
-        arr[i]의 값이 arr[i-1]과 다르다면 list에 넣어줌
-        
-        list의 값을 다시 answer에 넣어줌
-        */
 
-        List newList = new ArrayList();
-        newList.add(arr[0]);
         
-        for(int i = 1; i<arr.length; i++){
-            if(arr[i]!=arr[i-1]){
-                newList.add(arr[i]);
+        Stack<Integer> newStack = new Stack<>();
+        
+        newStack.push(arr[0]);
+        
+        for(int i=1; i<arr.length; i++){
+            if(newStack.peek() !=arr[i]){
+                newStack.push(arr[i]);
             }
         }
+        System.out.print(newStack.size());
         
-//         int[] answer = new int[newList.size()];
+        int[] answer=new int[newStack.size()];
         
-        
-//         for (int i = 0 ; i < newList.size() ; i++){
-//             answer[i] = Integer.parseInt(newList.get(i));
-
-//         }
-
-        int[] answer = new int[newList.size()];
-        for(int i=0; i<newList.size(); i++){
-            // System.out.print(newList.get(i)+" ");
-            answer[i] = (int)newList.get(i);
+        for(int i=answer.length-1; i>=0; i--){
+            answer[i]=newStack.pop();
         }
+        
         return answer;
 
     }
