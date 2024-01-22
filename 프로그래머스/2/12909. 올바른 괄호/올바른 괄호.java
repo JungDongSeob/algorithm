c83ada76c4ba2378117d13d3bcf0b65ad56f8e1e
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-01-23 00:02:01 +0900

    [level 2] Title: 올바른 괄호, Time: 13.67 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,67 +1,37 @@
-import java.util.*;
+import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int count = 0;
+
         /*
-        올바른 괄호이면 true
-        올바르지 않은 괄호이면 false
+        괄호의 짝이 맞아야한다.
+        괄호는 반드시 ")"로 끝나야한다.
         
-        괄호는 반드시 "("로 시작
-        괄호는 반드시 ")"로 끝
-        올바른 괄호는 반드시 짝지어서 끝나야됨
+        문자열 s를 배열로 나눈다.
+        "("를 만나면 push한다.
+        ")"를 만나면 pop한다.
         
-        (은 +1           )은 -1
-        단 한번이라도 -1이 되면 false
-        마지막에 0이 아니라면 false
-        그 외 true
+        만약 pop했는데 없을경우 올바르지 않은 괄호이다.
+        마지막에 스택의 size가 0이면 올바른 괄호이다.
         */
-        // String[] newArr = s.split("");
         
-
-//         for(int i=0; i<newArr.length; i++){
-//             if(newArr[i].equals("(")){
-//                 count++;
-//             }else{
-//                 count--;
-//             }
-//             if(count<0){
-//                 return false;
-//             }
-//         }
-
-//         if(count>0){
-//             return false;
-//         }
+        char[] charArr = s.toCharArray();
+        Stack<String> stackStr = new Stack<>();
         
-        char[] newArr = s.toCharArray();
-        // if(newArr[newArr.length-1].equals("(")){
-        //     return false;
-        // }
-        Stack<Character> stackStr = new Stack<>();
         try{
-
-            for(int i=0; i<newArr.length; i++){
-                if(newArr[i]==('(')){
-                    stackStr.push(newArr[i]);
-                    count++;
+            for(int i=0; i<charArr.length; i++){
+                if(charArr[i]=='('){
+                    stackStr.push("(");
                 }else{
                     stackStr.pop();
-                    count--;
                 }
             }
-        }catch(Exception e1){
+        }catch(Exception e){
             return false;
         }
-        // if(stackStr.peek().equals("(")){
-        //     return false;
-        // }
-        if(count!=0){
+        if(stackStr.size()!=0){
             return false;
         }
         return true;
-        
-
     }
-    
 }
