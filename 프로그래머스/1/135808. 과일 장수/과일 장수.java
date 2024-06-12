d6506f16f35eb6ef34908395be4adeb3c0c8eb77
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-06-12 23:35:03 +0900

    [level 1] Title: 과일 장수, Time: 0.36 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,27 +1,16 @@
-import java.util.PriorityQueue;
-import java.util.Collections;
+
+import java.util.*;
+
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int cnt = 0;
-        int a = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i = 0 ; i < score.length; i++){
-            pq.add(score[i]);
-        }
-        
-        while(pq.size() >= m){
-            for(int i = 0; i < m ; i ++){
-                a = pq.poll();
-                cnt++;
-                if(cnt==m){
-                    answer = answer + a * cnt;
-                    cnt = 0;
-                }
-            }
 
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
-        
+
         return answer;
     }
 }
