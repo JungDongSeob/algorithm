db0c69b25edfa109a52c5b7124e5e9d250671811
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-06-19 18:02:19 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,69 +1,39 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        int[] status = new int[n+2];
+        
+        for(int i = 0; i < lost.length; i++){
+            status[lost[i]] = -1;
+        }
+        for(int i = 0; i < reserve.length; i++){
+            status[reserve[i]] = status[reserve[i]] + 1; 
+        }
+        
+        for(int i = 1; i < status.length -1; i++){
+            if(status[i] == -1 && status[i-1] == 1){
+                status[i] = 0;
+                status[i-1] = 0;
+            }else if(status[i] == -1 && status[i+1] == 1){
+                status[i] = 0;
+                status[i+1] = 0;
+            }
+        }
+        for(int i = 1; i <status.length-1; i++){
+            if(status[i] == 0 || status[i] == 1){
+                answer++;
+            }
+        }
         /*
-        lost번호가 reserver 번호보다 1 작다면 같이 lost는 null
-        null이 아닌 개수 
+        학생수 만큼 status + 2
+        status[lost[i]] = -1
+        status[reserve[i]] = +1
+        
+        만약 status[n] == -1 이고 n-1이 +1이면
+            n = n + 1 , n-1 = n-1 +1
+        만약 status[n] == -1 이고 n+1이 +1이면
+            n = n + 1 , n+1 = n+1 -1
         */
-        
-        for(int i=1; i<reserve.length; i++){
-            for(int j=0; j<reserve.length - 1; j++){
-                if(reserve[i]<reserve[j]){
-                    int tmp = reserve[j];
-                    reserve[j] = reserve[i];
-                    reserve[i] = tmp;
-                }
-            }
-        }
-        
-        for(int i=1; i<lost.length; i++){
-            for(int j=0; j<lost.length - 1; j++){
-                if(lost[i]<lost[j]){
-                    int tmp = lost[j];
-                    lost[j] = lost[i];
-                    lost[i] = tmp;
-                }
-            }
-        }
-        
-         for(int i=0; i<reserve.length; i++){
-            for(int j=0; j<lost.length; j++){
-                if(reserve[i]==lost[j]){
-                    lost[j] = -4;
-                    reserve[i] = -2;
-                }
-            }
-         }
-        
-        for(int i=0; i<reserve.length; i++){
-            for(int j=0; j<lost.length; j++){
-                if(reserve[i]==lost[j] - 1 || reserve[i] == lost[j] + 1){
-                    lost[j] = -4;
-                    reserve[i] = -2;
-                }
-            }
-        }
-
-        // for(int i=0; i<reserve.length; i++){
-        //     for(int j=0; j<lost.length; j++){
-        //         if(reserve[i]==lost[j]-1){
-        //             lost[j] = -4;
-        //             reserve[i] = -2;
-        //         }
-        //     }
-        // }
-
-
-        int count = 0;
-        
-        for(int i=0; i<lost.length; i++){
-            if(lost[i]!=-4)
-                count++;
-        }
-        answer = n - count;
-        
-        // for(int i=0; i<lost.length; i++)
-        //     System.out.print(lost[i] + " ");
         
         return answer;
     }
