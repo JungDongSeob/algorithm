43f03cd339eccb592736fc2c4a41b9884440e17e
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-06-28 23:17:54 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,17 @@
+function solution(num) {
+
+    
+    if(num % 2 == 0){
+        answer = 'Even';
+    }else{
+        answer = 'Odd';
+    }
+    // let answer = (num)=>{
+    //     if(num%2 == 0){
+    //         return 'Even'
+    //     }
+    //     return 'Odd'
+    // };
+    
+    return answer;
+}
