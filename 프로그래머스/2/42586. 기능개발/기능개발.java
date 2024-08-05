b831073942ecc806e5e5b27fabe6a54b5550c8f7
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-08-05 18:37:10 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,71 +1,51 @@
 import java.util.Queue;
 import java.util.LinkedList;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-
+        int[] days = new int[speeds.length];
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> dayQ = new LinkedList<>();
         
+        for(int i = 0; i < speeds.length; i++){
+            int remainWork = 100 - progresses[i];
+            int work = remainWork / speeds[i];
+            
+            if(remainWork % speeds[i] > 0) work++;
+            
+            dayQ.offer(work);
+        }
         
-        /*
-        앞에 작업은 자유롭게 끝날 수 있음
-        하지만 배포는 앞에 작업이 배포되어야 뒤에 작업도 배포가능
-        배포는 하루에 한 번
-        하루에 몇 개의 작업이 배포되는지 return
-        
-        작업이 완료하는 시간인 time 배열 선언
-        앞의 작업이 완료되어야 뒤의 작업이 완료 되므로 Queue 선언
-        
-        Queue에 작업완료시간을 넣어줌
-        하나를 poll을 해서 새로운 변수에 넣어주고 peek를 통해 poll한 시간보다 낮으면 peek값도 poll
-        해주고 한번에 빠진 작업수를 리스트에 추가
-        다시 poll한게 peek값보다 작으면 peek값을 새로운 변수에 넣어주고 반복
-        */
-        
-        int[] time = new int[progresses.length];
-        int progressesTime = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> answerList = new ArrayList<>();
-        int count = 1;
-        int nd = 1;
-        for(int i=0; i< time.length; i++){
-
-            for(int days=1; days<=100; days++){
-                progresses[i] = progresses[i] + speeds[i];
-                if(progresses[i]>=100){
-                    time[i]=days;
-                    System.out.println(days);
+        while(!dayQ.isEmpty()){
+            int finish = dayQ.poll();
+            int cnt = 1;
+            while(!dayQ.isEmpty()){
+                if(dayQ.peek() <= finish){
+                    dayQ.poll();
+                    cnt++;
+                }else{
                     break;
                 }
             }
+            ans.add(cnt);
         }
         
-        for(int i=0; i<time.length; i++){
-            queue.add(time[i]);
+        int[] answer = new int[ans.size()];
+        
+        for(int i = 0; i < ans.size(); i++){
+            answer[i] = ans.get(i);
         }
         
-        progressesTime = queue.poll();
-        while(nd!=time.length){
-            if(progressesTime>=queue.peek()){
-                queue.poll();
-                count++;
-            }else{
-                answerList.add(count);
-                progressesTime = queue.poll();
-                count=1;
-            }
-            
-            if(nd==time.length-1){
-                answerList.add(count);
-            }
-            nd++;
-        }
-        
-        int[] answer = new int[answerList.size()];
-        
-        for(int i=answerList.size()-1; i>=0; i--){
-            answer[i]=answerList.get(i);
-        }
+        /*
+        완료까지 걸리는 일자를 큐에 순서대로 넣고
+        큐를 뽑고
+        cnt = 1
+        while
+        다음 큐가 첫번째 큐보다 작으면 cnt++
+        크면 answer 에 cnt 넣고 끝
+        */
         
         return answer;
     }
