8dd4a160a4e0691036c348c9c318b5f000240173
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-08-07 15:08:00 +0900

    [level 2] Title: 프로세스, Time: 3.38 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,83 +1,36 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
+        int cnt = 0;
+        boolean flag = true;
+        int[][] infos = new int[priorities.length][2];
+        Queue<int[]> q = new LinkedList<>();
         
-        /*
-        실행 대기 큐 > 꺼냄
-        대기 중인 큐의 우선순위가 방금 꺼낸거 보다 높으면 방금 꺼낸거 다시 넣음
-        꺼낸게 제일 크면 실행하고 꺼낸건 다시 안넣음
-        
-        우선순위와 boolean이 적힌 노드 생성
-        리스트에 다 넣고
-        마지막걸 꺼내서 새로운 노드에 저장하고 리스트 안에있는 값들과 비교해서 우선순위가 제일 크면
-        꺼내고 count ++
-        만약 boolean값이 true면 answer 제출
-        
-        아니라면 다시 첫번째에 삽입
-         
-        */
-        
-        List<Node> newList = new ArrayList<>();
-        for(int i=0; i<priorities.length; i++){
-            if(i==location){
-                // Node newNode = new Node(priorities[i], true);
-                newList.add(new Node(priorities[i], true));
-            }else{
-                // Node newNode = new Node(priorities[i], false);
-                newList.add(new Node(priorities[i], false));
-            }
-            // System.out.println(newList.get(i).getLocation());
+        for(int i = 0; i < priorities.length; i++){
+            q.offer(new int[]{priorities[i], i});
         }
-        /*
-        2 3 1 2
         
-        */
-        
-        // for(int k=0; k<newList.size(); k++){
-        //     System.out.print(newList.get(k).getPriorities());
-        // }
-        int count;
         while(true){
-            count = 1;
-            Node tmpNode = newList.remove(0);
-            for(int j=0; j<newList.size(); j++){
-                System.out.println(newList.get(j).getPriorities());
-                if(newList.get(j).getPriorities() >tmpNode.getPriorities()){
-                    newList.add(tmpNode);
-                    
-                    break;
-                }
-                count++;
-            }
-            if(count==newList.size()+1){
-                answer++;
-                if(tmpNode.getLocation()){
-                    break;
-                }
-            }
-               
+            int[] info = q.poll();
+            int lenF = q.size();
             
-            // System.out.println();
+            for(int[] arr : q){
+                if(arr[0] > info[0]){
+                    q.offer(new int[]{info[0],info[1]});
+                    break;
+                }
+            }
+            
+            int lenS = q.size();
+            if(lenF == lenS){
+                cnt++;
+                if(info[1] == location){
+                    return cnt;
+                }
+            }
+            
         }
-        
-        return answer;
     }
-}
- class Node{
-    private int priorities;
-    private boolean location;
-    
-    public int getPriorities(){
-        return  priorities;
-    }
-    
-    public boolean getLocation(){
-        return location;
-    }
-        
-    public Node(int priorities, boolean location) {
-		this.priorities = priorities;
-		this.location = location;
-	}
 }
