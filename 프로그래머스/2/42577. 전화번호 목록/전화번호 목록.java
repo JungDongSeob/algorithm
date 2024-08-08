2fc6c60e36b3e5d3a9d3d3b6e4be996df912d447
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-08-09 01:28:21 +0900

    [level 2] Title: 전화번호 목록, Time: 334.82 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,27 +1,16 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        /*
-        
-        
-        */
         Arrays.sort(phone_book);
-
-        for(int i=0; i<phone_book.length-1; i++){
-            for(int j=i-1; j<=i+1; j++){
-                if(i==j||j<0||i<0)
-                    continue;
-                if(phone_book[i].startsWith(phone_book[j])||phone_book[j].startsWith(phone_book[i])){
-                    return false;
-                }
+        
+        for(int i = 0; i < phone_book.length - 1; i++){
+            if(phone_book[i].length() <= phone_book[i+1].length() && phone_book[i].equals(phone_book[i+1].substring(0,phone_book[i].length()))){
+                return false;
             }
         }
-
-        
-        
-        
-        return true;
+        return answer;
     }
 }
