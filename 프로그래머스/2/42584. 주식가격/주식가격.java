4ede8613d4b327e65b17eb0a4c1cbe14b9244154
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-08-16 15:44:09 +0900

    [level 2] Title: 주식가격, Time: 67.12 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,35 +1,30 @@
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        /*
-        주식가격 배열 price
-        price[n]기준으로 price[n+i] <price[n]이면 answer[n] = i;
-        n+i가 prices 마지막이면 answer[n]=i 이고 answer[마지막] = 0
-        return 떨어지지 않은 기간
-
+        Stack<int[]> st = new Stack<>();
         
-        n   0   1   2   3   4
-        i       1   2   3   4
-        n   0   
-        i       
-            */
-        int second=0;
         
-        for(int n=0; n<prices.length-1; n++){
-            for(int i=n+1; i<prices.length; i++){
-                if(prices[n]>prices[i]||i==prices.length-1){
-                    // System.out.println("n : " + n + " i : " + i + " i - n : " + (i-n));
-                    if(n==2){
-                        System.out.println(n+" " + i);
-                    }
-                    answer[n]=i-n;
+        for(int i = 0; i < prices.length; i++){
+            while(!st.isEmpty()){
+                if(st.peek()[0] > prices[i]){
+                    answer[st.peek()[1]] = i - st.peek()[1];
+                    st.pop();
+                }else{
                     break;
                 }
             }
+            st.add(new int[] {prices[i], i});
         }
         
-        answer[prices.length-1] = 0;
+        int len = st.size();
+        for(int i = 0; i < len; i++){
+            answer[st.peek()[1]] = answer.length - st.peek()[1] - 1;
+            st.pop();
+        }
+        
         return answer;
     }
 }
