aa4614648154651a6c0497df5614aac82b9af397
JungDongSeob <58664027+JungDongSeob@users.noreply.github.com>
2024-08-19 21:16:47 +0900

    [level 2] Title: 택배상자, Time: 179.17 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,54 +2,25 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Stack;
 
-// class Solution {
-//     public int solution(int[] order) {
-//         int answer = 0;
-        
-//         Stack<Integer> st = new Stack<>();
-//         Queue<Integer> orderQ = new LinkedList<>();
-        
-//         for(int ni : order){
-//             orderQ.offer(ni);
-//         }
-        
-//         for(int i = 1; i <= order.length; i++){
-//             if(orderQ.peek() == i){
-//                 orderQ.poll();
-//                 answer++;
-//             }else{
-//                 st.push(i);
-//             }
-//             while(!st.isEmpty()){
-//                 if(orderQ.peek() == st.peek()){
-//                     orderQ.poll();
-//                     st.pop();
-//                     answer++;
-//                 }else{
-//                     break;
-//                 }
-//             }
-//         }
-        
-//         return answer;
-//     }
-// }
-
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
         
         Stack<Integer> st = new Stack<>();
-        int idx = 0;  // order 배열을 가리킬 인덱스
+        Queue<Integer> orderQ = new LinkedList<>();
+        
+        for(int ni : order){
+            orderQ.offer(ni);
+        }
         
         for(int i = 1; i <= order.length; i++){
-            if(order[idx] == i){
+            if(orderQ.peek().intValue() == i){
+                orderQ.poll();
                 answer++;
-                idx++;
-                while(!st.isEmpty() && st.peek() == order[idx]){
+                while(!st.isEmpty() && orderQ.peek().intValue() == st.peek().intValue()){
+                    orderQ.poll();
                     st.pop();
                     answer++;
-                    idx++;
                 }
             }else{
                 st.push(i);
@@ -59,3 +30,28 @@ class Solution {
         return answer;
     }
 }
+
+// class Solution {
+//     public int solution(int[] order) {
+//         int answer = 0;
+        
+//         Stack<Integer> st = new Stack<>();
+//         int idx = 0;  // order 배열을 가리킬 인덱스
+        
+//         for(int i = 1; i <= order.length; i++){
+//             if(order[idx] == i){
+//                 answer++;
+//                 idx++;
+//                 while(!st.isEmpty() && st.peek() == order[idx]){
+//                     st.pop();
+//                     answer++;
+//                     idx++;
+//                 }
+//             }else{
+//                 st.push(i);
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
